44ecb3bf6c640683535aaa95af2fc0174e460cfd
pmtempone <pmtempone@gmail.com>
2015-12-13 01:03:37 -0300

    plot 1

--- plot1.R
+++ plot1.R
@@ -4,4 +4,8 @@ x1[,1] <- as.Date(x1$Date,"%d/%m/%Y")
 
 x1_subset <- subset(x1,x1$Date>="2007-02-01" & x1$Date<="2007-02-02")
 
+png("plot1.png",width = 480,height = 480)
+hist(x1_subset$Global_active_power,main = "Global Active Power",xlab = "Global Active Power (kilowatts)",col = "red")
+dev.off()
+
 hist(x1_subset$Global_active_power,main = "Global Active Power",xlab = "Global Active Power (kilowatts)",col = "red")
