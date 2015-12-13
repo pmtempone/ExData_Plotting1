1784e59a2a4a50a2d1edf6a49d3ca50acc77ad6d
pmtempone <pmtempone@gmail.com>
2015-12-13 00:29:03 -0300

    Plot 1

---
+++ plot1.R
@@ -0,0 +1,7 @@
+read.table("~/Documentos/exploratory_data_analysis/household_power_consumption.txt",sep = ";",na.strings = "?",header = TRUE)
+
+x1[,1] <- as.Date(x1$Date,"%d/%m/%Y")
+
+x1_subset <- subset(x1,x1$Date>="2007-02-01" & x1$Date<="2007-02-02")
+
+hist(x1_subset$Global_active_power,main = "Global Active Power",xlab = "Global Active Power (kilowatts)",col = "red")
